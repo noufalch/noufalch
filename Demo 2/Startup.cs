e8fe27b0b25e4e543b2801935d6631da9085223d
noufal <noufal@DESKTOP-61EO12P>
2018-11-27 11:49:18 +0530

    Add project files.

---
+++ Demo 2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Demo_2.Startup))]
+namespace Demo_2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
